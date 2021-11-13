6e4f0e381d52c889491ebcead1b9105ca7e88c3d
Black_Penguin <dkstmdwnswns@gmail.com>
2021-11-13 22:39:14 +0900

    hp 관측 하셈

--- Assets/BlackPenguin/Scripts/GameManager.cs
+++ Assets/BlackPenguin/Scripts/GameManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class GameManager : Singleton<GameManager>
 {
     // Start is called before the first frame update
     void Start()
@@ -11,8 +11,11 @@ public class GameManager : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    public void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+
+        }
     }
 }
