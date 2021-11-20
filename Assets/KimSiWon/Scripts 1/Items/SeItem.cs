2deaca55e9609d3214db05cfc83493e719cbbb42
으악 <siwon4727@gmail.com>
2021-11-21 05:33:31 +0900

    먼데요

--- Assets/KimSiWon/Scripts 1/Items/SeItem.cs
+++ Assets/KimSiWon/Scripts 1/Items/SeItem.cs
@@ -4,11 +4,26 @@ using UnityEngine;
 
 public class SeItem : Interaction
 {
-    
+    float Cooltime = 0;
 
     protected override void Action()
     {
-       
+        if (Yak.ShopYak == true && Cooltime == 0)
+        {
+            
+            Entity[] friendly = FindObjectsOfType<Entity>();
+            foreach (Entity entity in friendly)
+            {
+                
+                if (entity.stat.type == StatInfo.Type.ENEMY)
+                {
+                    entity.stat.defence += 3;
+                }
+
+            }
+            Cooltime = 10;
+
+        }
     }
 
     void Start()
@@ -19,7 +34,7 @@ public class SeItem : Interaction
   
     void Update()
     {
-        
+        Cooltime -= Time.deltaTime;
        
     }
     
--- Assets/KimSiWon/Scripts 1/Items/VitaminItem.cs
+++ Assets/KimSiWon/Scripts 1/Items/VitaminItem.cs
@@ -2,8 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class VitaminItem : MonoBehaviour
+public class VitaminItem : Interaction
 {
+    public float Cooltime = 0;
+    
+    protected override void Action()
+    {
+        if(Vitamin.ShopVitamin == true&& Cooltime == 0)
+        {
+            
+            Entity[] friendly = FindObjectsOfType<Entity>();
+            foreach (Entity entity in friendly)
+            {
+                if (entity.stat.type == StatInfo.Type.FRIENDLY)
+                {
+                    entity._hp = +50;
+                }
+                Cooltime = 30;
+
+            }
+        }   
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +33,6 @@ public class VitaminItem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Cooltime -= Time.deltaTime;
     }
 }
